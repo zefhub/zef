c32ac65742531b9db89ca868fcacec7b7eee5b81
zeyadkhaled <monosec15@gmail.com>
2022-04-21 11:00:22 +0300

    Update FX.Transact handler to use commands instead of GD

--- zef/core/fx/graph_transactions.py
+++ zef/core/fx/graph_transactions.py
@@ -8,17 +8,20 @@ def graph_transaction_handler(eff: Effect):
         eff (Effect): Effect({
             "type": FX.TX.Transact,
             "target_graph": g1,
-            "graph_delta": some_g_delta
+            "commands": list_of_commands
         })
     """
 
-    from ..graph_delta import perform_transaction, filter_temporary_ids, unpack_receipt
+    from ..graph_delta_new import perform_transaction_commands, filter_temporary_ids, unpack_receipt
 
     if eff.d["target_graph"].graph_data.is_primary_instance:
-        receipt = perform_transaction(eff.d['graph_delta'], eff.d['target_graph'])
-    else:
-        from ..overrides import merge
-        receipt = merge(eff.d["graph_delta"], eff.d["target_graph"])
+        receipt = perform_transaction_commands(eff.d['commands'], eff.d['target_graph'])
+
+    # if eff.d["target_graph"].graph_data.is_primary_instance:
+    #     receipt = perform_transaction(eff.d['graph_delta'], eff.d['target_graph'])
+    # else:
+    #     from ..overrides import merge
+    #     receipt = merge(eff.d["graph_delta"], eff.d["target_graph"])
     
     # we need to forward this if it is in the effect
     if 'unpacking_template' in eff.d:
