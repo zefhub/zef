3f7bc99783d8865559d8ecaaff7876d6ee1fca44
zeyadkhaled <monosec15@gmail.com>
2022-04-21 17:03:43 +0300

    Add merge back to graph_transaction_handler

--- zef/core/fx/graph_transactions.py
+++ zef/core/fx/graph_transactions.py
@@ -16,12 +16,9 @@ def graph_transaction_handler(eff: Effect):
 
     if eff.d["target_graph"].graph_data.is_primary_instance:
         receipt = perform_transaction_commands(eff.d['commands'], eff.d['target_graph'])
-
-    # if eff.d["target_graph"].graph_data.is_primary_instance:
-    #     receipt = perform_transaction(eff.d['graph_delta'], eff.d['target_graph'])
-    # else:
-    #     from ..overrides import merge
-    #     receipt = merge(eff.d["graph_delta"], eff.d["target_graph"])
+    else:
+        from ..overrides import merge
+        receipt = merge(eff.d["commands"], eff.d["target_graph"])
     
     # we need to forward this if it is in the effect
     if 'unpacking_template' in eff.d:
