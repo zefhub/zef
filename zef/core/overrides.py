e0139619fe53789ba889a1cbf1289970101e2120
zeyadkhaled <monosec15@gmail.com>
2022-04-21 18:10:24 +0300

    Spoof GraphDelta serialization for merge

--- zef/core/overrides.py
+++ zef/core/overrides.py
@@ -9,12 +9,14 @@ def merge(obj, g : Graph, fire_and_forget : bool = False):
     from ..pyzef.main import merge as orig_merge
     from .serialization import serialize, deserialize
 
-    raise NotImplementedError(f"THIS NEEDS TO BE FIXED!")
-    # TODO Fix this to not depend on deprecated GraphDelta
-    # if type(obj) == GraphDelta:
-    #     ds = serialize(obj)
-    #     rs = orig_merge(ds, g, fire_and_forget)
-    #     receipt = deserialize(rs)
-    #     return receipt
-    # else:
-    #     return orig_merge(obj, g, fire_and_forget)
+    # TODO Make sure this doesn't break something else!
+    # The check below was done on GraphDelta. But for now the serialized behavior is spoofed.
+    # My worry is the else statement. When is it hit because how can we be sure that obj is serialized.
+    if type(obj) == list:
+        serialized =  {"_zeftype": "GraphDelta", "value": serialize(list(obj))}
+        rs =  orig_merge(serialized, g, fire_and_forget)
+        receipt = deserialize(rs)
+        return receipt
+    else:
+        return orig_merge(obj, g, fire_and_forget)
+
