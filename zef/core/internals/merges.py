9bca78492680b553c848d4ce109b729ccb49e294
zeyadkhaled <monosec15@gmail.com>
2022-04-21 18:11:09 +0300

    Change _graphdelta_merge to work with commands instead of GraphDelta

--- zef/core/internals/merges.py
+++ zef/core/internals/merges.py
@@ -3,13 +3,20 @@ def _graphdelta_merge(g, serialized_delta):
     """Don't call this explicitly. Only for the zefdb core."""
 
     from ..serialization import serialize, deserialize
-    from .. import Graph
-    from .._ops import run
+    from .. import Graph, Effect, FX
+    from .._ops import run, transact
 
     # Double check primary role here.
     # if g.
-    delta = deserialize(serialized_delta)
-
-    receipt = delta | g | run
+    # TODO The reason we are constructing an effect is that serialized 
+    # data contains the commands and not the actions.
+    # Can we somehow fix that?
+    commands = deserialize(serialized_delta)
+    # receipt = delta | transact[g] | run
+    receipt = Effect({
+            "type": FX.TX.Transact,
+            "target_graph": g,
+            "commands": commands
+    }) | run
 
     return serialize(receipt)
