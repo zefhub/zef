105b96dea2acaca43745a3d1dff3f317c5aee49f
Daniel Cocks <daniel.cocks@gmail.com>
2022-04-22 12:52:52 +1000

    Merge to always serialize before passing to C++

--- zef/core/overrides.py
+++ zef/core/overrides.py
@@ -9,14 +9,8 @@ def merge(obj, g : Graph, fire_and_forget : bool = False):
     from ..pyzef.main import merge as orig_merge
     from .serialization import serialize, deserialize
 
-    # TODO Make sure this doesn't break something else!
-    # The check below was done on GraphDelta. But for now the serialized behavior is spoofed.
-    # My worry is the else statement. When is it hit because how can we be sure that obj is serialized.
-    if type(obj) in {list, tuple}:
-        serialized =  serialize({"value": list(obj)})
-        rs =  orig_merge(serialized, g, fire_and_forget)
-        receipt = deserialize(rs)
-        return receipt
-    else:
-        return orig_merge(obj, g, fire_and_forget)
+    serialized = serialize(obj)
+    rs =  orig_merge(serialized, g, fire_and_forget)
+    receipt = deserialize(rs)
+    return receipt
 
