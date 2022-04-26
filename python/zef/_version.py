845e5fa7a40c16dcc23916e8ec98256ded763db3
Daniel Cocks <daniel.cocks@gmail.com>
2022-04-27 09:43:41 +1000

    Added prefix to versioneer taging

--- python/zef/_version.py
+++ python/zef/_version.py
@@ -43,7 +43,7 @@ def get_config():
     cfg = VersioneerConfig()
     cfg.VCS = "git"
     cfg.style = "pep440"
-    cfg.tag_prefix = ""
+    cfg.tag_prefix = "pyzef-"
     cfg.parentdir_prefix = "None"
     cfg.versionfile_source = "zef/_version.py"
     cfg.verbose = False
