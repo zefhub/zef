81bd984b0a892ccea071fa93cfd2d815e1b89b7d
Daniel Cocks <daniel.cocks@gmail.com>
2022-04-27 17:07:07 +1000

    Delete old files

--- core/src_cpp/core/butler/test_compiles.sh
+++
@@ -1,5 +0,0 @@
-INCLUDES=( -I../include -I../../../libraries/range-v3/include -I../../../libraries/doctest/doctest -I../websocketpp -I../asio-1.18.1/include -I../nlohmann_json/include )
-OTHERS=( -L. -lzef -lssl -lcrypto -lzstd -ggdb )
-# g++ -std=c++20 -lpthread test.cpp -o test
-g++ -std=c++17 -lpthread $INCLUDES $OTHERS test_network.cpp -o network
-# g++ -std=c++17 -lpthread $INCLUDES $OTHERS test.cpp -o test
