0e855c9f8be33db617f9b4f7cb9951fac46e5439
zeyadkhaled <monosec15@gmail.com>
2022-04-28 09:13:16 +0300

    Extend ValueTypes to match old list

--- python/zef/core/VT/__init__.py
+++ python/zef/core/VT/__init__.py
@@ -10,20 +10,40 @@ String = ValueType(type_name='String', constructor_func=str)
 List = ValueType(type_name='List', constructor_func=tuple)
 Dict = ValueType(type_name='Dict', constructor_func=dict)
 Set = ValueType(type_name='Set', constructor_func=set)
-EZefRef = ValueType(type_name='EZefRef', constructor_func=None)     # TODO: ctor
-EZefRefs = ValueType(type_name='EZefRefs', constructor_func=None)     # TODO: ctor
-EZefRefss = ValueType(type_name='EZefRefss', constructor_func=None)     # TODO: ctor
-ZefRef = ValueType(type_name='ZefRef', constructor_func=None)     # TODO: ctor
-ZefRefs = ValueType(type_name='ZefRefs', constructor_func=None)     # TODO: ctor
-ZefRefss = ValueType(type_name='ZefRefss', constructor_func=None)     # TODO: ctor
-Graph = ValueType(type_name='Graph', constructor_func=None)     # TODO: ctor
-GraphSlice = ValueType(type_name='GraphSlice', constructor_func=None)     # TODO: ctor
-FlatGraph = ValueType(type_name='FlatGraph', constructor_func=None)     # TODO: ctor
-ZefOp = ValueType(type_name='ZefOp', constructor_func=None)     # TODO: ctor
-Stream = ValueType(type_name='Stream', constructor_func=None)     # TODO: ctor
-TX = ValueType(type_name='TX', constructor_func=None)     # TODO: ctor
-DataFrame = ValueType(type_name='DataFrame', constructor_func=None)     # TODO: ctor
-GraphDelta = ValueType(type_name='GraphDelta', constructor_func=None)     # TODO: ctor
+EZefRef = ValueType(type_name='EZefRef', constructor_func=None)     
+EZefRefs = ValueType(type_name='EZefRefs', constructor_func=None)     
+EZefRefss = ValueType(type_name='EZefRefss', constructor_func=None)     
+ZefRef = ValueType(type_name='ZefRef', constructor_func=None)     
+ZefRefs   = ValueType(type_name='ZefRefs', constructor_func=None)     
+ZefRefss  = ValueType(type_name='ZefRefss', constructor_func=None)     
+Graph     = ValueType(type_name='Graph', constructor_func=None)     
+GraphSlice= ValueType(type_name='GraphSlice', constructor_func=None)     
+FlatGraph = ValueType(type_name='FlatGraph', constructor_func=None)     
+ZefOp     = ValueType(type_name='ZefOp', constructor_func=None)     
+Stream    = ValueType(type_name='Stream', constructor_func=None)     
+TX        = ValueType(type_name='TX',          constructor_func=None)     
+DataFrame = ValueType(type_name='DataFrame', constructor_func=None)     
+GraphDelta = ValueType(type_name='GraphDelta', constructor_func=None)     
+Time       = ValueType(type_name='Time',       constructor_func=None)
+AET        = ValueType(type_name='AET',        constructor_func=None)
+ET         = ValueType(type_name='ET',         constructor_func=None)
+RT         = ValueType(type_name='RT',         constructor_func=None)
+TX         = ValueType(type_name='TX',         constructor_func=None)
+BT         = ValueType(type_name='BT',         constructor_func=None)
+Enum       = ValueType(type_name='Enum',       constructor_func=None)
+LazyValue  = ValueType(type_name='LazyValue',  constructor_func=None) 
+Awaitable  = ValueType(type_name='Awaitable',  constructor_func=None) 
+Graph      = ValueType(type_name='Graph',      constructor_func=None) 
+Query      = ValueType(type_name='Query',      constructor_func=None) 
+DeltaQuery = ValueType(type_name='DeltaQuery', constructor_func=None) 
+Effect     = ValueType(type_name='Effect',     constructor_func=None) 
+Function   = ValueType(type_name='Function',   constructor_func=None)
+Record     = ValueType(type_name='Record',     constructor_func=None)       
+Error      = ValueType(type_name='Error',      constructor_func=None)
+UID        = ValueType(type_name='UID',        constructor_func=None)       
+BaseUID    = ValueType(type_name='BaseUID',    constructor_func=None)       
+ZefRefUID  = ValueType(type_name='ZefRefUID',  constructor_func=None)        
+EternalUID = ValueType(type_name='EternalUID', constructor_func=None)       
 
 Union = UnionClass()
 Intersection = IntersectionClass()
