fea6798828c4ca8d5a3ebcc72f6adfd6afa86dd7
Ulf <ulf@zefhub.io>
2022-06-08 11:11:25 +0800

    GraphSlice repr fix

--- python/zef/core/graph_slice.py
+++ python/zef/core/graph_slice.py
@@ -60,7 +60,7 @@ class GraphSlice:
         
         
     def __repr__(self):
-        return f"GraphSlice(graph_uid='{str(uid(Graph(self.tx)))}', time_slice={graph_slice_index(self)})"
+        return f"GraphSlice(graph_uid='{str(uid(Graph(self.tx)))}', index={graph_slice_index(self)})"
 
     def __eq__(self, other):        
         return (
