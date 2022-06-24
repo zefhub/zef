74c19bcb5b19babc2064e267f5c273efe30c80c0
Daniel Cocks <daniel.cocks@gmail.com>
2022-06-24 14:29:35 +1000

    Export statement in mmap.cpp too

--- core/src/mmap.cpp
+++ core/src/mmap.cpp
@@ -12,6 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+#include "export_statement.h"
+
 #ifdef ZEF_WIN32
 #include "mmap_windows.cpp"
 #else
