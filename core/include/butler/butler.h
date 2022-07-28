057c1148154f6f1f052faf5525cbd90d8c8b66a8
Daniel Cocks <daniel.cocks@gmail.com>
2022-07-28 14:02:16 +1000

    Forgot to bump the max protocol version

--- core/include/butler/butler.h
+++ core/include/butler/butler.h
@@ -215,7 +215,7 @@ namespace zefDB {
             // The protocol version chosen for communication. This may have to be autodetected in earlier versions.
             std::atomic_int zefdb_protocol_version = -1;
             constexpr static int zefdb_protocol_version_min = 4;
-            constexpr static int zefdb_protocol_version_max = 5;
+            constexpr static int zefdb_protocol_version_max = 6;
             AtomicLockWrapper auth_locker;
             std::string upstream_layout();
 
