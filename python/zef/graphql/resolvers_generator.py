5f8f4d1580f24e9293cbd8641e75e29b0fd082f3
zeyadkhaled <monosec15@gmail.com>
2022-07-29 08:59:27 +0300

    Move graphql to main namespace

--- python/zef/graphql/resolvers_generator.py
+++ python/zef/graphql/resolvers_generator.py
@@ -12,8 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from ...core import *
-from ...ops import *
+from ..core import *
+from ..ops import *
 from ariadne import ObjectType,MutationType,QueryType,InterfaceType,SubscriptionType
 
 
