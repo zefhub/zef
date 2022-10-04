a5f60b2a6700ad053209887264c281761929b07a
Daniel Cocks <daniel.cocks@gmail.com>
2022-10-05 09:28:43 +1100

    Set/Dict/List fixes

--- python/zef/core/VT/extended_containers.py
+++ python/zef/core/VT/extended_containers.py
@@ -34,10 +34,8 @@ def list_is_a(x, typ):
     ab = typ._d['subtype']
     if isinstance(x, (Generator, ZefGenerator)):
         raise NotImplementedError()
-    if len(ab)!=1:    # List takes only one Type argument
-        print('Something went wrong in `is_a[List[...]]`: multiple args curried into ', file=sys.stderr)
 
-    return all(isinstance(item, ab[0]) for item in x)
+    return all(isinstance(item, ab) for item in x)
 make_VT('List',
         constructor_func=tuple,
         is_a_func=list_is_a,
@@ -47,13 +45,11 @@ def set_is_a(x, typ):
     import sys
     if not isinstance(x, PySet):
         return False
-    ab = typ._d['absorbed']
-    if len(ab) == 0:
+    if 'subtype' not in typ._d:
         return True
+    ab = typ._d['subtype']
 
-    if len(ab)!=1:    # List takes only one Type argument
-        print(f'Something went wrong in `is_a[Set[T1]]`: Set takes exactly one subtype, but got {x}', file=sys.stderr)
-    return all(isinstance(item, ab[0]) for item in x)
+    return all(isinstance(item, ab) for item in x)
 make_VT('Set', pytype=set, is_a_func=set_is_a,
         get_item_func=generic_subtype_get_item)
 
@@ -62,15 +58,15 @@ def dict_is_a(x, typ):
     import sys
     if not isinstance(x, PyDict):
         return False
-    ab = typ._d['absorbed']
-    if len(ab) == 0:
+    if 'subtype' not in typ._d:
         return True
+    ab = typ._d['subtype']
 
-    if (len(ab)!=1 or len(ab[0])!=2):    # Dict must contain a type in one [] and that must be a pair
+    if (len(ab)!=2):    # Dict must contain a type in one [] and that must be a pair
         print(f'Something went wrong in `is_a[Dict[T1, T2]]`: exactly two subtypes must be given. Got {ab}', file=sys.stderr)
         return Error('Error matching Dict[...]')
 
-    T1, T2 = ab[0]
+    T1, T2 = ab
     return all(isinstance(key, T1) and isinstance(val, T2) for key,val in x.items())
 make_VT('Dict', pytype=dict, is_a_func=dict_is_a,
         get_item_func=generic_subtype_get_item)
