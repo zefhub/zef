5073a25cc3e1869f9055ae6d57c7d4a9546c087b
Ulf <ulf@zefhub.io>
2022-11-28 07:27:21 +0800

    ZExpressions added, with debugging output

--- python/zef/core/__init__.py
+++ python/zef/core/__init__.py
@@ -95,6 +95,7 @@ from .zef_functions import func
 from .serialization import serialize, deserialize
 
 from .symbolic_expression import SV, SVs, V
+from .z_expression import ZZ    # TODO: rename this to "Z" and replace the ZefOp "Z"
 
 # Implementations come last, so that they can make use of everything else
 from . import op_implementations
