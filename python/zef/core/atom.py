3b8a85af63cc925b9050ff5f881531e5a27e14ef
zeyadkhaled <monosec15@gmail.com>
2023-01-09 08:37:57 +0300

    Allow Atom to wrap ZefRef/EZefRef

--- python/zef/core/atom.py
+++ python/zef/core/atom.py
@@ -23,29 +23,52 @@ class Atom_:
     """
 
     def __init__(self, atom_type, *names, **fields):
+        from ._ops import is_a, rae_type, uid
+        from .VT import ZefRef, EZefRef, Entity
+
+        ref_pointer = None
+        if is_a(atom_type, ZefRef) or is_a(atom_type, EZefRef):
+            assert is_a(atom_type, Entity), "Atom was called with a ZefRef not of EntityType"
+            # This means we can extract the atom_type and uid from the Ref
+            ref_pointer = atom_type
+            atom_type = rae_type(ref_pointer)
+            names =  (str(uid(ref_pointer)), *names)
+
         object.__setattr__(self, "atom_type", atom_type)
         object.__setattr__(self, "names", names)
         object.__setattr__(self, "fields", fields)
+        object.__setattr__(self, "ref_pointer", ref_pointer)
 
     def __call__(self, *args, **fields):
+        # TODO Add checks on passed *args to ensure they are valid names or accepted values
         new_fields = dict(object.__getattribute__(self, "fields"))
         new_fields.update(fields)
         names = object.__getattribute__(self, "names")
-        new_names = names + args
+        ref_pointer = object.__getattribute__(self, "ref_pointer")
+
+        if ref_pointer:
+            # In case ref_pointer is defined remove the first name "uid" as it gets added when we __init__ a new Object
+            new_names = names[1:] + args
+        else:
+            new_names = names + args
+
         return Atom_(
-            object.__getattribute__(self, "atom_type"),
+            # If ref_pointer is defined then pass that instead of atom_type
+            ref_pointer if ref_pointer else object.__getattribute__(self, "atom_type"),
             *new_names,
             **new_fields
         )
 
 
     def __repr__(self):
+        ref_pointer = get_ref_pointer(self)
         atom_type = get_atom_type(self)
         names = get_names(self)
         fields = get_fields(self)
         items = [f'"{get_reference_type(self)}"'] + [f"{k}={v!r}" for k,v in fields.items()]
         items = names + tuple(items)
-        return f'{atom_type}({f", ".join(items)})'
+        ref_pointer_str = f" -> {ref_pointer}" if ref_pointer else ""
+        return f'{atom_type}({f", ".join(items)}){ref_pointer_str}'
 
     def __setattr__(self, name, value):
         raise AttributeError("Atoms are immutable")
@@ -70,8 +93,8 @@ def get_names(atom: Atom):
     return object.__getattribute__(atom, "names")
 def get_fields(atom: Atom):
     return object.__getattribute__(atom, "fields")
-        
-
+def get_ref_pointer(atom: Atom):
+    return object.__getattribute__(atom, "ref_pointer")
 
 def get_uid_type(uid_str: str) -> str:
     uid_chunk_size = 12
