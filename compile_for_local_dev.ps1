fbf816ffe090e200d80504aac0ef64e219d2e5e6
Daniel Cocks <daniel.cocks@gmail.com>
2023-01-09 11:06:08 +1100

    Windows powershell local compile

---
+++ compile_for_local_dev.ps1
@@ -0,0 +1,28 @@
+#!/bin/bash
+# Copyright 2022 Synchronous Technologies Pte Ltd
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+$ErrorActionPreference="Stop"
+
+cd python\pyzef
+
+mkdir -Force build
+cd build
+
+cmake .. -DCMAKE_BUILD_TYPE=Release "-DZef_DIR=$((Resolve-Path ../../../core).ToString())" -DLIBZEF_PYZEF_BUNDLED=TRUE -DLIBZEF_STATIC=TRUE -DLIBZEF_FORCE_ASSERTS=TRUE
+
+cmake --build . --config Release -j
+
+copy Release\pyzef.* ..\..\zef\
+copy ..\..\..\core\auth.html ..\..\zef\
